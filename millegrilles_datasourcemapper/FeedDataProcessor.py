8adc2ee6ae466cac1547d91d07a4cb1198f63c6f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-05-15 15:22:11 -0400

    Parsing individual items from staged file

--- millegrilles_datasourcemapper/FeedDataProcessor.py
+++ millegrilles_datasourcemapper/FeedDataProcessor.py
@@ -1,12 +1,36 @@
+import asyncio
 import logging
+import json
+import gzip
 
 from millegrilles_datasourcemapper.FeedViewProcessor import ProcessJob
+
+class FeedDataItem:
+
+    def __init__(self, data: str, files: dict):
+        self.data = data
+        self.files = files
+
+    @staticmethod
+    def from_str(line: str):
+        value = json.loads(line)
+        data = value['data']
+        files = value.get('files')
+        return FeedDataItem(data, files)
 
 
 class FeedViewDataProcessor:
 
     def __init__(self, job: ProcessJob):
         self._job = job
+
+    async def read_data_items(self):
+        with gzip.open(self._job.data_file_path, 'rt') as fp:
+            while True:
+                line = await asyncio.to_thread(fp.readline, 1024 * 1024 * 16)  # Max 16 mb per record
+                if len(line) == 0:
+                    return
+                yield FeedDataItem.from_str(line)
 
     async def process(self):
         raise NotImplementedError('must implement')
@@ -19,6 +43,11 @@ class FeedViewDataProcessorPythonCustom(FeedViewDataProcessor):
 
     async def process(self):
         self.__logger.debug("Processing data")
+        count = 0
+        async for item in self.read_data_items():
+            pass
+            count += 1
+        self.__logger.debug(f"Parsed through {count} data items")
 
 
 def select_data_processor(job: ProcessJob) -> FeedViewDataProcessor:
@@ -26,5 +55,5 @@ def select_data_processor(job: ProcessJob) -> FeedViewDataProcessor:
     if feed_type == 'web.scraper.python_custom':
         return FeedViewDataProcessorPythonCustom(job)
     else:
-        Exception('Feed type not supported')
+        raise Exception('Feed type not supported')
 
